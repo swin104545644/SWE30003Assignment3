8cd7e12e0e1626dbffb56a34c581b19a443a2bf9
swin104545644 <104545644@student.swin.edu.au>
2025-11-07 23:01:11 +1100

    Updating delivery

--- Models/DeliveryMethod.cs
+++ Models/DeliveryMethod.cs
@@ -4,7 +4,7 @@ namespace OnlineShop.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool isActive { get; set; } = true;
+        public bool IsActive { get; set; } = true;
         public decimal BasePrice { get; set; } = 0m;
         public decimal PerKmPrice { get; set; } = 0.10m;
         public decimal FreeOverAmount { get; set; } = 75m;
